53f9727e871f879af127fb4e0653b91e81dcde6e
zaldirizald <125567889+zaldirizald@users.noreply.github.com>
2023-03-06 10:22:28 +0700

    New

---
+++ 11_Basic Linux Command/Praktikum/ShellScript1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hallo, Selamat datang di contoh shell script Rizaldi!"
+echo "Hari ini tanggal:"
+date
+echo "Anda Sedang Login sebagai User:"
+whoami
+
+echo "proses yang sedang berjalan dikomputer ini adalah:"
+ps aux
+
+echo "Disk Usage pada komputer ini adalah:"
+df -h
+
+
